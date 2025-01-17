33648c7be826356c36bfeb6c69a9224685b9d955
John Lilly <jm2lilly@gmail.com>
2025-01-17 16:22:57 -0500

    Adjust file paths and inline script

--- src/admin/index.js
+++ src/admin/index.js
@@ -2,4 +2,4 @@ import CMS from "decap-cms-app";
 import "/src/admin/preview-style.css"; // Adjust the path if needed
 
 // Register the custom preview style
-CMS.registerPreviewStyle("/src/admin/preview-style.css");
+CMS.registerPreviewStyle("./src/admin/preview-style.css");
--- src/scripts/main.js
+++
@@ -1,4 +0,0 @@
-// Set current year in site footer
-document.getElementById("currentYearFooter").textContent = new Date().getFullYear();
- 
-  
