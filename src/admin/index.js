446a54faae0763586b433d7661fa24761412ec0c
John Lilly <jm2lilly@gmail.com>
2025-01-17 16:32:14 -0500

    Update index.js

--- src/admin/index.js
+++ src/admin/index.js
@@ -1,5 +1,6 @@
 import CMS from "decap-cms-app";
-import "/src/admin/preview-style.css"; // Adjust the path if needed
+import "/admin/preview-style.css"; // Adjust the path if needed
 
 // Register the custom preview style
-CMS.registerPreviewStyle("./src/admin/preview-style.css");
+CMS.registerPreviewStyle("/admin/preview-style.css");
+console.log("CMS preview script loaded!");
