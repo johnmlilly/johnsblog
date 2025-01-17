c1f19098489526d03bc97eefab06b2915b84e32f
John Lilly <jm2lilly@gmail.com>
2025-01-17 16:05:42 -0500

    create custom css for cms preview

---
+++ src/admin/index.js
@@ -0,0 +1,5 @@
+import CMS from "decap-cms-app";
+import "./styles/preview-style.css"; // Adjust the path if needed
+
+// Register the custom preview style
+CMS.registerPreviewStyle("/styles/preview-style.css");
