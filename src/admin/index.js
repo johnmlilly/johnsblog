0ce3bec0bb1b42e311405a14779180c6d52fc208
John Lilly <jm2lilly@gmail.com>
2025-01-17 17:04:07 -0500

    Remove comment in index.js

--- src/admin/index.js
+++ src/admin/index.js
@@ -3,4 +3,3 @@ import "/admin/preview-style.css"; // Adjust the path if needed
 
 // Register the custom preview style
 CMS.registerPreviewStyle("/admin/preview-style.css");
-console.log("CMS preview script loaded!");
