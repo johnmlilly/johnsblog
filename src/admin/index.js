7d93d46bb318e3d77615d87a771a79fa0f083a6c
John Lilly <jm2lilly@gmail.com>
2025-01-17 16:12:06 -0500

    correct file paths

--- src/admin/index.js
+++ src/admin/index.js
@@ -1,5 +1,5 @@
 import CMS from "decap-cms-app";
-import "./styles/preview-style.css"; // Adjust the path if needed
+import "/src/styles/preview-style.css"; // Adjust the path if needed
 
 // Register the custom preview style
-CMS.registerPreviewStyle("/styles/preview-style.css");
+CMS.registerPreviewStyle("/src/styles/preview-style.css");
