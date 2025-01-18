4c8b3f633053b07dc3f704f0b34221b416541a34
John Lilly <jm2lilly@gmail.com>
2025-01-17 20:00:32 -0500

    Inline CMS script

--- src/admin/index.js
+++
@@ -1,5 +0,0 @@
-import CMS from "decap-cms-app";
-import "/admin/preview-style.css"; // Adjust the path if needed
-
-// Register the custom preview style
-CMS.registerPreviewStyle("/admin/preview-style.css");
