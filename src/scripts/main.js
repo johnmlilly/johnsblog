0e98e08136cd34152720a45a731d65d80361edbb
John Lilly <jm2lilly@gmail.com>
2025-02-04 10:39:58 -0500

    Create code header element

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,21 +2,33 @@
 // **** Copy and paste code from code elements
 // Reference: https://www.roboleary.net/demos/iNGpDikU/preview/?tab=code
 
-const copyButtonIcon = '<i class="bi bi-clipboard-check"></i>';
+const copyButtonIcon = '<i class="bi bi-copy"></i>&nbsp;Copy';
 // only pre elements that have a code as a child
 let blocks = document.querySelectorAll("pre:has(code)");
 
 blocks.forEach((block) => {
-  // only add button if browser supports Clipboard API
-  if (navigator.clipboard) {
-    let button = document.createElement("button");
-    button.innerHTML = copyButtonIcon;
-    block.appendChild(button);
+    let codeElement = block.querySelector("code");
 
-    button.addEventListener("click", async () => {
-      await copyCode(block, button);
-    });
-  }
+    // Extract language from the class (e.g., "language-javascript")
+    let languageClass = [...codeElement.classList].find(cls => cls.startsWith("language-"));
+    let language = languageClass ? languageClass.replace("language-", "") : "Code Snippet";
+
+    // Create and insert a header into the pre element
+    let codeHeader = document.createElement("div");
+    codeHeader.innerText = language.toUpperCase();
+    codeHeader.classList.add("code-language-header");
+
+    block.insertBefore(codeHeader, codeElement);
+    // only add button if browser supports Clipboard API
+    if (navigator.clipboard) {
+        let button = document.createElement("button");
+        button.innerHTML = copyButtonIcon;
+        codeHeader.appendChild(button);
+
+        button.addEventListener("click", async () => {
+        await copyCode(block, button);
+        });
+    }
 });
 
 async function copyCode(block, button) {
@@ -26,7 +38,7 @@ async function copyCode(block, button) {
   await navigator.clipboard.writeText(text);
 
   // visual feedback that task is completed
-  button.innerHTML = '<i class="bi bi-check-circle"></i>'
+  button.innerHTML = '<i class="bi bi-check-circle"></i>&nbsp;Copied!'
 
   setTimeout(() => {
     button.innerHTML = copyButtonIcon;
