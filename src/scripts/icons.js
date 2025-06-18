d354d8238599299d3e1dac29840d6385f59c195e
John Lilly <jm2lilly@gmail.com>
2025-06-17 21:30:56 -0400

    Import lucide with tree shaking

---
+++ src/scripts/icons.js
@@ -0,0 +1,10 @@
+// Recommended way, to include only the icons you need.
+import { createIcons, Copy , CircleCheck, Mail, } from 'lucide';
+
+createIcons({
+  icons: {
+        Copy,
+        CircleCheck,
+        Mail,
+  }
+});
