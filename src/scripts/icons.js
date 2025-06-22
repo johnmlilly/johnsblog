64728b6d2810f985e25394d0a453eb3d362eb840
John Lilly <jm2lilly@gmail.com>
2025-06-22 12:47:23 -0400

    Add more icons and pics

--- src/scripts/icons.js
+++ src/scripts/icons.js
@@ -1,10 +1,17 @@
 // Recommended way, to include only the icons you need.
-import { createIcons, Copy , CircleCheck, Mail, } from 'lucide';
+import { createIcons, Copy , CircleCheck, Mail, Code, Server, PencilRuler, Palette, Lightbulb, Calendar, Clock, } from 'lucide';
 
 createIcons({
   icons: {
         Copy,
         CircleCheck,
         Mail,
+        Code,
+        Server,
+        PencilRuler,
+        Palette,
+        Lightbulb,
+        Calendar,
+        Clock
   }
 });
