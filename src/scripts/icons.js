2776ef0fbdf128119b3d0b89f64c659aba21b16c
John Lilly <jm2lilly@gmail.com>
2025-10-03 11:37:37 -0400

    add new icon

--- src/scripts/icons.js
+++ src/scripts/icons.js
@@ -1,5 +1,5 @@
 // Recommended way, to include only the icons you need.
-import { createIcons, Copy , CircleCheck, Mail, Code, Server, PencilRuler, Palette, Lightbulb, Calendar, Clock, } from 'lucide';
+import { createIcons, Copy , CircleCheck, Mail, Code, Server, PencilRuler, Palette, Lightbulb, Calendar, Clock, BookOpenCheck } from 'lucide';
 
 createIcons({
   icons: {
@@ -12,6 +12,7 @@ createIcons({
         Palette,
         Lightbulb,
         Calendar,
-        Clock
+        Clock,
+        BookOpenCheck
   }
 });
